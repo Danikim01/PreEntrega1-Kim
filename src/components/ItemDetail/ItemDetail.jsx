fd6dbce13ae8bbc03084e23f08ab18098cb5d1f5
Danikim01 <dakim@fi.uba.ar>
2023-12-22 01:04:33 -0300

    detail pages added, spinner removed

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,16 +1,33 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
+import {useState,useEffect} from 'react'
 import './ItemDetail.css'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
 
 const ItemDetail = () => {
+    const [item, setItem] = useState([])
     const {id} = useParams()
+
+    useEffect(() => {
+        axios.get(`https://fakestoreapi.com/products/${id}`).then(res => setItem(res.data))
+    }, [id])
+
     return (
     <>
-        <h1>{id}</h1>
-        <div className='tarjetaProducto'>
-            <button>Agregar al Carrito</button>
-        </div>
+        {
+            <div className='tarjetaProducto'>
+                <img className='imgItem' src={item.image} alt={item.title}/>
+                <div className='datosProducto'>
+                    <h1>{item.title}</h1>
+                    <p>{item.description}</p>
+                    <h3>Price: ${item.price}</h3>
+                    <button>Agregar al Carrito</button>
+                </div>
+            </div>
+        }
+        
     </>
   )
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -4,30 +4,25 @@ import {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-
 import './ItemListContainer.css'
 import UserCard from '../UserCard/UserCard'
-import Spinner from '../Spinner/Spinner'
+
 
 const ItemListContainer = ({greeting}) => {
   const [items, setItems] = useState([])
-  const [loading, setLoading] = useState(true)
 
   let { categoryId } = useParams();
 
   useEffect(() => {
-    setLoading(true)
     axios.get('https://fakestoreapi.com/products')
     .then(res => {
       setItems(res.data)
     })
-    setLoading(false)
   }, [])
 
   return (
     <>
-      {loading ? 
-      <Spinner/> : 
+      {
       <>
         <h1>{categoryId ? categoryId.charAt(0).toUpperCase() + categoryId.slice(1) : greeting}</h1>
         <div className='Card-List'>
